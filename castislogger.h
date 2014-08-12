87d5163a5579b69bcb6454604b558c311131e6fe
mnpk <mwpark@castis.com>
2014-08-12 10:55:26 +0900

    refectoring

--- castislogger.h
+++ castislogger.h
@@ -14,13 +14,10 @@
 #include <boost/filesystem/fstream.hpp>
 #include <boost/filesystem/operations.hpp>
 
-namespace logging = boost::log;
-namespace src = boost::log::sources;
-namespace expr = boost::log::expressions;
-namespace sinks = boost::log::sinks;
-
 #define CILOG(severity)\
-  BOOST_LOG_SEV(my_logger::get(), severity) << boost::filesystem::path(__FILE__).filename().string() << "::" << __FUNCTION__ << ":" << __LINE__ << ",,"
+  BOOST_LOG_SEV(my_logger::get(), severity)\
+  << boost::filesystem::path(__FILE__).filename().string()\
+  << "::" << __FUNCTION__ << ":" << __LINE__ << ",,"
 
 enum severity_level
 {
@@ -36,6 +33,7 @@ enum severity_level
   critical
 };
 
+// The operator is used for regular stream formatting
 std::ostream& operator<< (std::ostream& strm, severity_level level)
 {
   static const char* strings[] =
@@ -61,8 +59,10 @@ std::ostream& operator<< (std::ostream& strm, severity_level level)
 }
 
 struct severity_tag;
-logging::formatting_ostream& operator<< (logging::formatting_ostream& strm,
-    logging::to_log_manip<severity_level, severity_tag> const& manip)
+// The operator is used when putting the severity level to log
+boost::log::formatting_ostream& operator<< (
+    boost::log::formatting_ostream& strm,
+    boost::log::to_log_manip<severity_level, severity_tag> const& manip)
 {
   static const char* strings[] =
   {
@@ -88,7 +88,9 @@ logging::formatting_ostream& operator<< (logging::formatting_ostream& strm,
 }
 
 class cilog_backend:
-  public sinks::basic_formatted_sink_backend<char, sinks::synchronized_feeding>
+  public boost::log::sinks::basic_formatted_sink_backend<
+    char,
+    boost::log::sinks::synchronized_feeding>
 {
   private:
     bool auto_flush_;
@@ -112,17 +114,19 @@ class cilog_backend:
         characters_written_(0)
     {
     }
-    void consume(logging::record_view const& rec, string_type const& formatted_message)
+
+    void consume(
+        boost::log::record_view const& /*rec*/,
+        string_type const& formatted_message)
     {
-      if (
-          (file_.is_open() &&
-           (characters_written_ + formatted_message.size() >= rotation_size_)
-           ) ||
-        !file_.good()
-        )
+      if ((file_.is_open() &&
+           (characters_written_ + formatted_message.size() >= rotation_size_))
+          ||
+          (!file_.good()))
       {
         rotate_file();
       }
+
       if (!file_.is_open())
       {
         file_path_ = generate_filepath();
@@ -130,75 +134,101 @@ class cilog_backend:
         file_.open(file_path_);
         if (!file_.is_open())
         {
+          // failed to open file
           return;
         }
         characters_written_ = static_cast<std::streamoff>(file_.tellp());
       }
-      file_.write(formatted_message.data(), static_cast<std::streamsize>(formatted_message.size()));
+      file_.write(
+          formatted_message.data(),
+          static_cast<std::streamsize>(formatted_message.size()));
       file_.put('\n');
       characters_written_ += formatted_message.size() + 1;
-      if (auto_flush())
+
+      if (auto_flush_)
       {
         file_.flush();
       }
     }
+
+  private:
     void rotate_file()
     {
       file_.close();
       file_.clear();
       characters_written_ = 0;
     }
-    bool auto_flush()
-    {
-      return auto_flush_;
-    }
+
     boost::filesystem::path generate_filepath()
     {
-      std::locale locale_ymd(std::cout.getloc(), new boost::posix_time::time_facet("%Y-%m-%d"));
-      std::stringstream filename_prefix_ss;
-      filename_prefix_ss.imbue(locale_ymd);
-      filename_prefix_ss << boost::posix_time::second_clock::universal_time();;
+      std::string filename_prefix = datetime_string_with_format("%Y-%m-%d");
+      std::string monthly_path_name = datetime_string_with_format("%Y-%m");
 
-      std::locale locale_ym(std::cout.getloc(), new boost::posix_time::time_facet("%Y-%m"));
-      std::stringstream monthly_path_ss;
-      monthly_path_ss.imbue(locale_ym);
-      monthly_path_ss << boost::posix_time::second_clock::universal_time();;
-      boost::filesystem::path monthly_target_path(target_path_ / monthly_path_ss.str());
+      boost::filesystem::path monthly_path(target_path_ / monthly_path_name);
+      // e.g. 2014-08-12[1]_example.log
+      boost::regex pattern(
+          filename_prefix + "[\\[\\]0-9]*" + "_" + file_name_suffix_ + ".log");
+      uintmax_t next_index = scan_next_index(monthly_path, pattern);
 
-      uintmax_t file_index = 0;
-      boost::regex pattern(filename_prefix_ss.str() + "[\\[\\]0-9]*" + "_" + file_name_suffix_ + ".log");
-      if (boost::filesystem::exists(monthly_target_path) && boost::filesystem::is_directory(monthly_target_path))
+      std::stringstream filename_ss;
+      filename_ss << filename_prefix;
+      if (next_index > 0)
       {
-        boost::filesystem::directory_iterator it(monthly_target_path), end;
+        filename_ss << "[" << next_index << "]";
+      }
+      filename_ss << "_" + file_name_suffix_;
+      filename_ss << ".log";
+
+      return boost::filesystem::path(monthly_path / filename_ss.str());
+    }
+
+    std::string datetime_string_with_format(std::string const& format)
+    {
+      std::locale loc(
+          std::cout.getloc(),
+          new boost::posix_time::time_facet(format.c_str()));
+      std::stringstream ss;
+      ss.imbue(loc);
+      ss << boost::posix_time::second_clock::universal_time();;
+      return ss.str();
+    }
+
+    uintmax_t scan_next_index(
+        boost::filesystem::path const& path,
+        boost::regex const& pattern)
+    {
+      uintmax_t next_index = 0;
+      if (boost::filesystem::exists(path) &&
+          boost::filesystem::is_directory(path))
+      {
+        boost::filesystem::directory_iterator it(path), end;
         for (; it != end; ++it)
         {
           if (boost::regex_match(it->path().filename().string(), pattern))
           {
-            std::string filename = it->path().filename().string();
-            size_t pos_index_begin = filename.find('[');
-            size_t pos_index_end = filename.find(']');
-            unsigned int index = 0;
-            if (pos_index_begin != std::string::npos && pos_index_end != std::string::npos)
+            uintmax_t index = parse_index(it->path().filename().string());
+            if (index >= next_index)
             {
-              index = atoi(filename.substr(pos_index_begin + 1, pos_index_end - pos_index_begin).c_str());
-            }
-            if (index >= file_index)
-            {
-              file_index = index + 1;
+              next_index = index + 1;
             }
           }
         }
       }
+      return next_index;
+    }
 
-      std::stringstream filename_ss;
-      filename_ss << filename_prefix_ss.str();
-      if (file_index > 0)
+    uintmax_t parse_index(std::string const& filename)
+    {
+      size_t pos_index_begin = filename.find('[');
+      size_t pos_index_end = filename.find(']');
+      unsigned int index = 0;
+      if (pos_index_begin != std::string::npos &&
+          pos_index_end != std::string::npos)
       {
-        filename_ss << "[" << file_index << "]";
+        index = atoi(filename.substr(pos_index_begin + 1,
+                     pos_index_end - pos_index_begin).c_str());
       }
-      filename_ss << "_" + file_name_suffix_;
-      filename_ss << ".log";
-      return boost::filesystem::path(monthly_target_path / filename_ss.str());
+      return index;
     }
 };
 
@@ -211,24 +241,32 @@ namespace castis
         const std::string& app_version,
         const std::string& target,
         unsigned long long rotation_size = 100 * 100 * 1024,
-        bool auto_flush = true
-        )
+        bool auto_flush = true)
     {
-      logging::add_common_attributes();
-      typedef sinks::synchronous_sink< cilog_backend > sink_t;
-      boost::shared_ptr<sink_t> sink(new sink_t(boost::filesystem::path(target), app_name, rotation_size, auto_flush));
+      namespace expr = boost::log::expressions;
+      boost::log::add_common_attributes();
+      typedef boost::log::sinks::synchronous_sink< cilog_backend > sink_t;
+      boost::shared_ptr<sink_t> sink(
+          new sink_t(boost::filesystem::path(target),
+            app_name,
+            rotation_size,
+            auto_flush));
       sink->set_formatter(
           expr::stream
           << app_name
           << "," << app_version
-          << "," << expr::format_date_time<boost::posix_time::ptime>("TimeStamp", "%Y-%m-%d,%H:%M:%S.%f")
+          << ","
+          << expr::format_date_time<boost::posix_time::ptime>(
+            "TimeStamp",
+            "%Y-%m-%d,%H:%M:%S.%f")
           << "," << expr::attr<severity_level, severity_tag>("Severity")
-          << "," << expr::smessage
-          );
-      logging::core::get()->add_sink(sink);
+          << "," << expr::smessage);
+      boost::log::core::get()->add_sink(sink);
     }
   }
 }
 
-BOOST_LOG_INLINE_GLOBAL_LOGGER_DEFAULT(my_logger, src::severity_logger_mt<severity_level>)
+BOOST_LOG_INLINE_GLOBAL_LOGGER_DEFAULT(
+    my_logger,
+    boost::log::sources::severity_logger_mt<severity_level>)
 
